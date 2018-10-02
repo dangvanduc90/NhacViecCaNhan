99d1d955a62ee2689c2107cbc6d021d83bb153a1
dangvanduc90 <dangvanduc0@gmail.com>
2018-10-02 22:07:58 +0700

    Alarm snooze,remove AlarmActivity

--- app/src/main/java/com/example/chung/nhacvieccanhan/helpers/AlarmHelper.java
+++ app/src/main/java/com/example/chung/nhacvieccanhan/helpers/AlarmHelper.java
@@ -4,21 +4,16 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 
-import com.example.chung.nhacvieccanhan.data.SQLite;
 import com.example.chung.nhacvieccanhan.model.CongViec;
 import com.example.chung.nhacvieccanhan.model.SongService;
 import com.example.chung.nhacvieccanhan.receiver.AlarmReceiver;
-import com.example.chung.nhacvieccanhan.ultils.ConstClass;
 import com.example.chung.nhacvieccanhan.ultils.UtilLog;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import static com.example.chung.nhacvieccanhan.ultils.ConstClass.EXTRA_ON_OF;
 import static com.example.chung.nhacvieccanhan.ultils.ConstClass.INTENT_ID_CONGVIEC;
@@ -27,7 +22,6 @@ import static com.example.chung.nhacvieccanhan.ultils.ConstClass.ON;
 
 public class AlarmHelper {
     private static final String TAG = "AlarmHelper";
-    static SQLite db;
 
     public static void deleteAlarm(Context mContext, CongViec congViec) {
         AlarmManager alarmManager;
@@ -66,5 +60,35 @@ public class AlarmHelper {
     }
 
     public static void SnoozeAlarm(Context mContext, CongViec congViec) {
+        AlarmManager alarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(mContext, SongService.class);
+        intent.putExtra(EXTRA_ON_OF, ON);
+        intent.putExtra(INTENT_ID_CONGVIEC, congViec.getId());
+        PendingIntent pendingIntent = PendingIntent.getService(
+                mContext, (int) congViec.getId(), intent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        Date mDate = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            mDate = dateFormat.parse(congViec.getNgay());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        int hours = Integer.parseInt(congViec.getThoigian().split(":")[0]);
+        int minutes = Integer.parseInt(congViec.getThoigian().split(":")[1]);
+        mDate.setHours(hours);
+        mDate.setMinutes(minutes);
+        long currentTime = mDate.getTime();
+        long nextTime = currentTime + congViec.getThoiGianLap() * 60000;
+
+//        Calendar cal = Calendar.getInstance();
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm a");
+//        cal.setTimeInMillis(nextTime);
+//        String date = sdf.format(cal.getTime());
+//        UtilLog.log_d(TAG, congViec.getThoiGianLap() + "");
+//        UtilLog.log_d(TAG, date);
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, nextTime, pendingIntent);
     }
 }
